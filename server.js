217af7f3d231fdeb88d8e47b0e1795bbd97a37fe
DimitrisK <dimitris.kaikonidis@gmail.com>
2021-05-21 17:26:48 +0200

    early draft - need to implement canvas

---
+++ server.js
@@ -0,0 +1,26 @@
+const chalk = require("chalk");
+const express = require("express");
+const hb = require("express-handlebars");
+const { getSignatures, addSignatures } = require("./db");
+
+const app = express();
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get("/", (req, res) => res.redirect("petition"));
+
+app.get("/petition", (req, res) => {
+    res.render("petition");
+});
+
+// app.post("/petition", (req, res) => {
+//     addSignatures("omg, 13, 2").then(result => {
+//         console.log(result.rows);
+//         res.send("OK");
+//     });
+// });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Running Server @ ${chalk.blue(`http://localhost:${PORT}`)}`));
